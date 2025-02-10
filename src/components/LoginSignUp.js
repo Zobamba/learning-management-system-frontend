23b068c9d17bd96f05e5d282a2f04f72995b5273
Onah Chizoba Bernard <onahbernardchizoba@gmail.com>
2025-02-10 16:12:57 +0100

    connect to backend

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 
 const LoginSIgnUp = ({ closeModal }) => {
   const token = localStorage.getItem("token");
+
+  const logOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    window.location.reload();
+    closeModal();
+  };
   return (
     <div
       className="flex flex-col items-center justify-center fixed inset-0 w-full h-full bg-black bg-opacity-50 z-[999]"
@@ -12,9 +19,7 @@ const LoginSIgnUp = ({ closeModal }) => {
         className="absolute top-[10%] right-[5%] flex flex-col space-y-2 p-4 rounded-md bg-gray-100 shadow-sm"
         onClick={(e) => e.stopPropagation()}
       >
-        {token ? (
-          ""
-        ) : (
+        {!token && (
           <>
             <Link
               className="flex items-center justify-center h-[2.3rem] w-[13rem] rounded-full bg-gray-200 hover:bg-gray-300 text-[#891C69] font-medium text-[15px] font-montserrat leading-[1.295rem] text-center"
@@ -42,6 +47,16 @@ const LoginSIgnUp = ({ closeModal }) => {
         >
           TOP RATED COURSES ⭐️
         </Link>
+        {token && (
+          <>
+            <button
+              onClick={logOut}
+              className="flex items-center justify-center h-[2.3rem] w-[13rem] rounded-full bg-gray-200 hover:bg-gray-300 text-[#891C69] font-medium text-[15px] font-montserrat leading-[1.295rem] text-center"
+            >
+              LOGOUT
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
